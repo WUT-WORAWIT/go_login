82b1843ab6accc163f731bec41b24b1e7be96366
worawit <woravit1357@gmail.com>
2024-04-26 13:09:47 +0700

    new

--- Database/connect.go
+++ Database/connect.go
@@ -8,7 +8,8 @@ import (
 )
 
 func Init() *gorm.DB {
-	dsn := "host=localhost user=postgres password=123456 dbname=TEST port=5433 sslmode=disable TimeZone=Asia/Shanghai"
+	// dsn := "host=127.0.0.1 user=postgres password=123456 dbname=TEST port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "host=172.17.0.2 user=postgres database=TEST password=banana port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -17,4 +18,11 @@ func Init() *gorm.DB {
 	// db.AutoMigrate(&Models.User{})
 
 	return db
+}
+func Close(db *gorm.DB) {
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer sqlDB.Close()
 }
---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# เลือก base image สำหรับการเริ่มต้น
+FROM golang:1.21.1
+
+# ติดตั้ง PostgreSQL
+# RUN apt-get update && apt-get install -y postgresql postgresql-contrib
+
+# กำหนด working directory
+WORKDIR /app
+
+# คัดลอกไฟล์ go.mod และ go.sum สำหรับโปรเจ็กต์
+COPY go.mod go.sum ./
+
+# ดาวน์โหลดและติดตั้ง dependencies
+RUN go mod download
+
+# คัดลอกโค้ดทั้งหมดไปยัง working directory
+COPY . .
+
+# Compile แอปพลิเคชัน
+RUN go build -o main .
+
+# กำหนด port ที่แอปพลิเคชันจะใช้
+EXPOSE 8080
+
+# กำหนดคำสั่งที่จะใช้เมื่อ container ถูกเริ่มต้น
+CMD ["./main"]
